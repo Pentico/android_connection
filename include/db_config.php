a78387d557036417eb8ffc40a603fd4b80563111
Pentico <morulaneat@gmail.com>
2016-09-19 10:47:40 +0200

    db_config.php - Has the connection details for database

---
+++ include/db_config.php
@@ -0,0 +1,14 @@
+db_config.php
+<?php
+
+/*
+ * All database connection variables
+ */
+
+//TODO  Add the required details for connection
+
+define('DB_USER', ""); // db user
+define('DB_PASSWORD', ""); // db password (mention your db password here)
+define('DB_DATABASE', ""); // database name
+define('DB_SERVER', ""); // db server
+?>
